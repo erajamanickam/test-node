85a57e73ec443f7ccaa60c147ec784a27fe80f2d
Rajamanickam <rajamanickam.e@xerago.com>
2023-06-14 20:05:27 +0530

    move file views

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-// const fs = require("fs");
+const fs = require("fs");
 // set the view engine to ejs
 
 // Set the view engine and views directory
@@ -12,7 +12,32 @@ app.set('views', './views');
 
 // index page
 app.get('/', function(req, res) {
-  res.render('login');
+  // res.render('login');
+  const viewsDirectory = path.join(__dirname, 'var/task');
+
+// // Check if the views directory exists
+// if (!fs.existsSync(viewsDirectory)) {
+//   // Create the views directory
+//   fs.mkdirSync(viewsDirectory);
+//   console.log('Var directory created successfully.');
+// } else {
+//   console.log('var directory already exists.');
+// }
+
+
+const sourceFilePath = path.join(__dirname, 'login.ejs');
+const destinationFilePath = path.join(__dirname, 'var/task', 'login.ejs');
+
+// Move the file to the views directory
+fs.rename(sourceFilePath, destinationFilePath, (err) => {
+  if (err) {
+    console.error('Error moving file:', err);
+  } else {
+    console.log('File moved successfully.');
+  }
+});
+
+
 });
 
 app.get('/api', function(req, res) {
@@ -24,7 +49,7 @@ app.get('/api', function(req, res) {
       res.status(200).send(filename, 'Root directory')
     );
 
-    fs.readdir("var/", (err,filename)=>console.log(filename, 'views directory'))
+    fs.readdir("var/", (err,filename)=>console.log(filename, 'var directory'))
     console.log(__dirname, 'test');
   });
 
